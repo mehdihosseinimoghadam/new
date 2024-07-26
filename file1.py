88906963344086b8fc595710c8cee0b8f48ab0ab
mehdihosseinimoghadam <m.h.moghadam1996@gmail.com>
2024-07-26 09:35:48 +0100

    feat: feature1 3

--- file1.py
+++ file1.py
@@ -1,25 +1,30 @@
 import numpy as np
+
 def function1():
     # Function 1 code here
-    result = 2 + 3
+    result = np.sum(np.arange(1000))
     print("Result of function1:", result)
 
 def function2(x):
     # Function 2 code here
-    result = 5 - 2 + x
+    result = np.prod(np.arange(1, x+1))
     print("Result of function2:", result)
 
 def function3(x, y):
     # Function 3 code here
     s = function2(3)
-    result = 3+x * y^7 + s
+    result = np.power(x, y) + np.sum(np.arange(s))
     print("Result of function3:", result)
 
 def main():
     # Call the functions here
     function1()
-    function2()
-    function3(3,7, 6)
+    function2(5)
+    function3(3, 7)
 
 if __name__ == "__main__":
     main()
+    def function4():
+        # Function 4 code here
+        result = np.mean(np.random.randint(1, 100, size=10))
+        print("Result of function4:", result)
