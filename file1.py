782d69823e8926de1099c7eef6649ce5dcf199eb
Mehdi Hosseini Moghadam <m.h.moghadam1996@gmail.com>
2024-07-26 09:21:36 +0100

    feat: 1 main

---
+++ file1.py
@@ -0,0 +1,23 @@
+def function1():
+    # Function 1 code here
+    result = 2 + 3
+    print("Result of function1:", result)
+
+def function2():
+    # Function 2 code here
+    result = 5 - 2
+    print("Result of function2:", result)
+
+def function3():
+    # Function 3 code here
+    result = 4 * 6
+    print("Result of function3:", result)
+
+def main():
+    # Call the functions here
+    function1()
+    function2()
+    function3()
+
+if __name__ == "__main__":
+    main()
